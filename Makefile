0e2a746893f0c35c6394366d0d7f35881222a7c2
Gabriel Falcão <gabriel@nacaolivre.org>
2015-02-04 14:00:31 -0500

    agile tools!

---
+++ Makefile
@@ -0,0 +1,16 @@
+all: install_deps test
+
+export PYTHONPATH := ${PWD}:${PYTHONPATH}
+
+
+install_deps:
+	@python setup.py develop
+
+test:
+	@nosetests -s --verbosity=2 tests --rednose
+
+clean:
+	git clean -Xdf
+
+release:
+	@python setup.py sdist register upload
---
+++ setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+
+install_requires = [
+    'mock',
+    'nose',
+    'rednose',
+    'sure',
+    'httpretty',
+]
+
+
+if __name__ == '__main__':
+    setup(
+        name='agile',
+        version='1.0.0',
+        description=(
+            'A meta-package containing a full toolset for agile development with TDD'
+        ),
+        author='Gabriel Falcao',
+        author_email='gabriel@nacaolivre.org',
+        include_package_data=True,
+        url='http://github.com/gabrielfalcao/agile',
+        packages=[],
+        install_requires=install_requires,
+    )
